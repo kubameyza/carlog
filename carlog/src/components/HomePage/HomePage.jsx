54410a962546750543cf277c9e54939dcda2b9b8
Jakub Meyza <kubameyza@gmail.com>
2024-01-22 14:48:16 +0100

    Add horizontal scroller

--- carlog/src/components/HomePage/HomePage.jsx
+++ carlog/src/components/HomePage/HomePage.jsx
@@ -2,11 +2,12 @@ import Counters from '../Counters/Counters';
 import Gallery from '../Gallery/Gallery';
 import Headline from '../Headline/Headline';
 import Offer from '../Offer/Offer';
+import Scroller from '../Scroller/Scroller';
 import styles from './HomePage.module.css';
 
 const HomePage = () => {
   return (
-    <>
+    <div className={styles.wrapper}>
       <div className={styles.homepage}>
         <div className={styles.car}>
           <div className={styles.lights}>
@@ -18,8 +19,9 @@ const HomePage = () => {
       </div>
       <Counters />
       <Gallery />
+      <Scroller />
       <Offer />
-    </>
+    </div>
   );
 };
 export default HomePage;
---
+++ carlog/src/components/Scroller/Scroller.jsx
@@ -0,0 +1,35 @@
+import styles from './Scroller.module.css';
+import { nanoid } from 'nanoid';
+
+const services = [
+  'Mycie i Woskowanie Premium',
+  'Pełne Oczyszczanie Wnętrza',
+  'Usuwanie Rys i Poprawa Lakieru',
+  'Detailing Felg i Opon',
+  'Ochronna Powłoka Ceramiczna',
+  'Ozonowanie Wnętrza',
+  'Detailing Skóry',
+  'Detailing Silnika',
+];
+
+const Scroller = () => {
+  return (
+    <div className={styles.wrapper}>
+      <ul className={styles.scroller}>
+        {services.map((item) => (
+          <li className={styles.scroller_element} key={nanoid}>
+            {item}
+          </li>
+        ))}
+      </ul>
+      <ul className={styles.scroller}>
+        {services.map((item) => (
+          <li className={styles.scroller_element} key={nanoid}>
+            {item}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+export default Scroller;
