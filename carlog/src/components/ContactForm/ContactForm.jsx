efe1df2e73d6e1ce83539586a49ebfbace2747db
Jakub Meyza <kubameyza@gmail.com>
2024-02-15 11:32:07 +0100

    Add form validation

--- carlog/src/components/ContactForm/ContactForm.jsx
+++ carlog/src/components/ContactForm/ContactForm.jsx
@@ -1,31 +1,48 @@
+import toast from 'react-hot-toast';
 import style from './ContactForm.module.css';
 
 const ContactForm = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    e.target.reset();
+    toast.success('Twoja wiadomość została wysłana!');
+  };
+
   return (
-    <form className={style.wrapper}>
+    <form className={style.wrapper} onSubmit={(e) => handleSubmit(e)}>
       <input
         name="name"
         className={style.field}
         type="text"
         placeholder="Imię i Nazwisko"
+        required
       />
-      <input className={style.field} type="text" placeholder="Email" />
+      <input
+        className={style.field}
+        type="email"
+        placeholder="Email"
+        required
+      />
       <input
         name="phone"
         className={style.field}
-        type="text"
+        type="tel"
         placeholder="Telefon kontaktowy"
+        required
+        pattern="[0-9]{9}"
       />
       <input
         name="car"
         className={style.field}
         type="text"
         placeholder="Samochód"
+        required
       />
       <textarea
         name="message"
         className={style.big_field}
         placeholder="Twoja wiadomość"
+        required
       />
       <div className={style.button_wrapper}>
         <button className={style.button} type="submit">
--- carlog/src/components/Layout/Layout.jsx
+++ carlog/src/components/Layout/Layout.jsx
@@ -2,9 +2,11 @@ import { Outlet } from 'react-router-dom';
 import Header from '../Header/Header';
 import style from './Layout.module.css';
 import Footer from '../Footer/Footer';
+import { Toaster } from 'react-hot-toast';
 const Layout = () => {
   return (
     <div className={style.layout}>
+      <Toaster />
       <div className={style.header}>
         <Header />
       </div>
